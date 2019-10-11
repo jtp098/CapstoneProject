b837e1614540551cc7ea4c8cdae181fc38a71fc9
John p <jpelliccia@gmail.com>
2019-10-10 22:45:43 -0400

    CP-31 Sign-up Page Layout

--- Teamerizer/src/app/app.component.ts
+++ Teamerizer/src/app/app.component.ts
@@ -20,6 +20,11 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
+    },
+    {
+      title: 'SignUp',
+      url: '/sign-up-page',
+      icon: 'list'
     }
   ];
 
