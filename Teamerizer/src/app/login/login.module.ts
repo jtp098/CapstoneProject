8e76602a15e8adce4c85fc2e23e8e69cbec5b52c
John p <jpelliccia@gmail.com>
2019-10-26 15:02:23 -0400

    Updating Login page so that it uses forms

--- Teamerizer/src/app/login/login.module.ts
+++ Teamerizer/src/app/login/login.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+
 
 import { IonicModule } from '@ionic/angular';
 
@@ -18,7 +19,9 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
+    
     RouterModule.forChild(routes)
   ],
   declarations: [LoginPage]
