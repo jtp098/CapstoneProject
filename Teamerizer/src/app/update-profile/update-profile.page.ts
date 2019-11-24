99fab3c3b60c1a2c88192a18f2cb13773b86e323
JohnMads <johnpmadias@gmail.com>
2019-11-23 19:35:56 -0500

    use picker to select skill and level

--- Teamerizer/src/app/update-profile/update-profile.page.ts
+++ Teamerizer/src/app/update-profile/update-profile.page.ts
@@ -5,6 +5,8 @@ import { firestore } from 'firebase/app';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { PickerController } from '@ionic/angular';
+import { PickerOptions } from '@ionic/core';
 
 @Component({
   selector: 'app-update-profile',
@@ -12,12 +14,14 @@ import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/fo
   styleUrls: ['./update-profile.page.scss'],
 })
 export class UpdateProfilePage implements OnInit {
+  skill = '';
+  selected = ['', '', ''];
 
   mainuser: AngularFirestoreDocument
 
     firstname: string
     lastname: string
-    skills: string
+    skillType: string
     skillLevel: string
     username: string
     busy: boolean = false
@@ -30,19 +34,17 @@ export class UpdateProfilePage implements OnInit {
     private afs: AngularFirestore,
 		private alertController: AlertController,
     private router: Router,
-    public user: UserService) { 
+    public user: UserService,
+    private pickerCtrl: PickerController) { 
       this.mainuser = afs.doc(`users/${user.getUID()}`)
       this.sub = this.mainuser.valueChanges().subscribe(event => {
         this.username = event.username
         this.firstname = event.firstName
         this.lastname = event.lastName
-        this.skills = event.skills
+        this.skillType = event.skillType
         this.skillLevel = event.skillLevel
 
-        })
-
-      
-
+      })
 
     }
 
@@ -51,7 +53,54 @@ export class UpdateProfilePage implements OnInit {
 
   ngOnDestroy() {
 		//this.sub.unsubscribe()
-	}
+  }
+
+  async showAdvancedPicker() {
+    let opts: PickerOptions = {
+      cssClass: 'academy-picker',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Done',
+          cssClass: 'special-done'
+        }
+      ],
+      columns: [ 
+        {
+          name: 'skillType',
+          options: [
+            { text: 'Angular', value: 'Angular' },
+            { text: 'Vue', value: 'Vue' },
+            { text: 'React', value: 'React' }
+          ]
+        },
+        {
+          name: 'skillLevel',
+          options: [
+            { text: 'Beginner', value: 'Beginner' },
+            { text: 'Intermediate', value: 'Intermediate' },
+            { text: 'Advanced', value: 'Advanced' }
+          ]
+        }
+      ]
+    };
+
+    let picker = await this.pickerCtrl.create(opts);
+    picker.present();
+    picker.onDidDismiss().then(async data => {
+      let skillType = await picker.getColumn('skillType');
+      let skillLevel = await picker.getColumn('skillLevel');
+      console.log('col: ', skillType);
+      this.selected = [
+        skillType.options[skillType.selectedIndex].value,
+        skillLevel.options[skillLevel.selectedIndex].value
+      ]
+    });
+
+  }
 
   async presentAlert(title: string, content: string) {
 		const alert = await this.alertController.create({
@@ -65,40 +114,26 @@ export class UpdateProfilePage implements OnInit {
 
   async updateProfile(){
     this.busy = true
-
-      
-		
 			//await this.user.updatefirstName(this.firstName)
 			this.mainuser.update({
-        
-      
 
         firstName: this.firstname,
-       lastName: this.lastname, 
-        skills: this.skills, 
-        skillLevel:this.skillLevel
-
-
-        
+        lastName: this.lastname, 
+        skillType: this.selected[0], 
+        skillLevel:this.selected[1]
 			})
 		
       if(this.newpassword){
         await this.user.updatePassword(this.newpassword)
     }
 
-
-
     this.password = ""
     this.newpassword = ""
-
 		this.busy = false
 
 		await this.presentAlert('Done!', 'Your profile was updated!')
 
 		this.router.navigate(['/home'])
-
-
-
 
   }
 
