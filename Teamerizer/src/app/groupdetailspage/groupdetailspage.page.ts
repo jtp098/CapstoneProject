8c35fdcb2310f4823da3b1037afebc1874fa4a45
Vijay <vg54639n@pace.edu>
2019-12-14 00:15:22 -0500

    Changed html and ts file

--- Teamerizer/src/app/groupdetailspage/groupdetailspage.page.ts
+++ Teamerizer/src/app/groupdetailspage/groupdetailspage.page.ts
@@ -11,142 +11,148 @@ import {forEach} from "@angular-devkit/schematics";
 
 
 @Component({
-    selector: 'app-groupdetailspage',
-    templateUrl: './groupdetailspage.page.html',
-    styleUrls: ['./groupdetailspage.page.scss'],
+	selector: 'app-groupdetailspage',
+	templateUrl: './groupdetailspage.page.html',
+	styleUrls: ['./groupdetailspage.page.scss'],
 })
 export class GroupdetailspagePage implements OnInit {
-		public userList: any[];
-		public loadedUserList: any[];
-		public useridtodel;
+	public userList: any[];
+	public loadedUserList: any[];
 	userinfo$ :any[];
 	selectedGrpName:any;
-		groupUsers = [];
+	groupUsers = [];
 
-		sliderConfig = {
-			spaceBetween: 10,
-			centeredSlides: true,
-			slidesPerView: 1.8
-		}
-
-    uid: string;
-    selectedName;
-    group$;
-    grouponSelectedname$;
-    grouplist: any;
-    constructor(private navCtrl: NavController,
-                public afstore: AngularFirestore,
-                public afAuth: AngularFireAuth,
-								public alertController: AlertController,
-								private userService: UserService,
-								private router: Router
-                ) {}
-
-    ngOnInit() {
-         this.afAuth.authState.subscribe(user => {
-            if(user){
-                this.getAllGroupsCreatedByCurrentUser(user.uid).subscribe(data => {
-                    console.log("Group List Data:", data);
-                    this.group$ = data;
-                });
-            }
-				});
-				this.afstore.collection('users').valueChanges().subscribe(userList => {
-					this.userList = userList;
-					this.loadedUserList = userList;
-					console.log("userlist",userList);
-				})
-
-				this.groupUsers = this.userService.getGroupUsers();
-		}
-
-		openDetailsWithState(firstName: string) {
-			let navigationExtras: NavigationExtras = {
-			  state: {
-				user: firstName
-			  }
-			};
-			this.router.navigate(['/member-details'], navigationExtras);
-		  }
-		
-		intializeItems(): void {
-			this.userList = this.loadedUserList;
-		}
-
-		filterList(evt) {
-			this.intializeItems();
-
-			const searchTerm = evt.srcElement.value;
-
-			if(!searchTerm) {
-				return;
-			}
-
-			this.userList = this.userList.filter(currentUser => {
-				if(currentUser.firstName && searchTerm) {
-					if (currentUser.firstName.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
-						return true;
-					}
-					return false;
-				}
-			});
-		}
-
-		addToGroup(user) {
-			this.useridtodel = user;
-			console.log(user+ "Users");
-			//this.userService.addUsers(user);
-			this.afstore.collection('users', ref => ref.where('uid', '==', user)).valueChanges().subscribe(userList => {
-				this.userinfo$ = userList;
-				console.log("user list add",this.userinfo$);
-			})
-			for(var userinfoobs$ in this.userinfo$) {
-				console.log(this.userinfo$[userinfoobs$].firstName+""+this.userinfo$[userinfoobs$].lastName)
-				this.afstore.collection('adduserstogrp').add({
-					firstName: this.userinfo$[userinfoobs$].firstName,
-					lastName: this.userinfo$[userinfoobs$].lastName,
-					//skills: this.userinfo$[userinfoobs$].skills,
-					grpname: this.selectedGrpName,
-					addflag: true,
-					uid: user,
-
-				})
-
-			}
-		}
-
-    groupDetailsDisplay(event) {
-    	this.selectedGrpName=event.target.value;
-        console.log("selected" + event.target.value)
-        this.getDetails(event.target.value).subscribe(data => {
-            this.grouponSelectedname$ = data;
-        });
-    }
-	async delay(ms: number) {
-		await new Promise(resolve => setTimeout(()=>resolve(), ms)).then(()=>console.log("fired"));
+	sliderConfig = {
+		spaceBetween: 10,
+		centeredSlides: true,
+		slidesPerView: 1.8
 	}
-remove(no){
-let size;
-	this.delay(5000).then(any=>{
-		this.afstore.collection('adduserstogrp', ref => ref.where('uid', '==', this.useridtodel)).valueChanges().subscribe(data => {
-			console.log('remove'+this.useridtodel);
-			console.log(data);
-			if(data){
-				(this.userList).splice(no, 1);
-			} });
-	});
-    }
 
-    getAllGroupsCreatedByCurrentUser(uid): Observable<any> {
-        return this.afstore.collection<any>('grouplist', ref => ref.where('createdBy', '==', uid)).valueChanges();
-    }
-    getDetails(grpName): Observable<any> {
-        return this.afstore.collection<any>('grouplist', ref => ref.where('groupname', '==', grpName)).valueChanges();
+	uid: string;
+	selectedName;
+	group$;
+	grouponSelectedname$;
+	grouplist: any;
+	constructor(private navCtrl: NavController,
+				public afstore: AngularFirestore,
+				public afAuth: AngularFireAuth,
+				public alertController: AlertController,
+				private userService: UserService,
+				private router: Router
+	) {}
+
+	ngOnInit() {
+		this.afAuth.authState.subscribe(user => {
+			if(user){
+				this.getAllGroupsCreatedByCurrentUser(user.uid).subscribe(data => {
+					console.log("Group List Data:", data);
+					this.group$ = data;
+				});
+			}
+		});
+		this.afstore.collection('users').valueChanges().subscribe(userList => {
+			this.userList = userList;
+			this.loadedUserList = userList;
+			console.log("userlist",userList);
+		})
+
+		this.groupUsers = this.userService.getGroupUsers();
+	}
+
+	openDetailsWithState(firstName: string) {
+		let navigationExtras: NavigationExtras = {
+			state: {
+				user: firstName
+			}
+		};
+		this.router.navigate(['/member-details'], navigationExtras);
+	}
+
+	intializeItems(): void {
+		this.userList = this.loadedUserList;
+	}
+
+	filterList(evt) {
+		this.intializeItems();
+
+		const searchTerm = evt.srcElement.value;
+
+		if(!searchTerm) {
+			return;
 		}
+
+		this.userList = this.userList.filter(currentUser => {
+			if(currentUser.firstName && searchTerm) {
+				if (currentUser.firstName.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+					return true;
+				}
+				return false;
+			}
+		});
+	}
+	 delay(ms: number) {
+		return new Promise( resolve => setTimeout(resolve, ms) );
+	}
+	 async addToGroup(user) {
+		 console.log(user + "Users");
+		 let observableUser$ = null;
+		 try {
+			 this.afstore.collection('users', ref => ref.where('uid', '==', user.uid)).valueChanges().subscribe((data) => {
+				 observableUser$ = data;
+				 this.userinfo$=observableUser$;
+				 console.log(this.userinfo$[0]);
+			 });
+		 } finally {
+			 await this.delay(2000);
+			 console.log("Fianl"+observableUser$);
+			 debugger;
+			 this.afstore.collection('users', ref => ref.where('uid', '==', user.uid)).valueChanges().subscribe((data) => {
+					 observableUser$ = data;
+					 console.log(observableUser$);
+				 }
+			 );
+		 }
+		 for(var userinfoobs$ in this.userinfo$) {
+			 console.log(this.userinfo$[userinfoobs$].firstName+""+this.userinfo$[userinfoobs$].lastName)
+			 this.afstore.collection('adduserstogrp').add({
+				 firstName: this.userinfo$[userinfoobs$].firstName,
+				 lastName: this.userinfo$[userinfoobs$].lastName,
+				 skillLevel: this.userinfo$[userinfoobs$].skillLevel,
+				 skillType: this.userinfo$[userinfoobs$].skillType,
+				 grpname: this.selectedGrpName,
+				 addflag: true,
+				 uid: user.uid,
+			 })
+		 }
+		 this.afstore.collection('adduserstogrp', ref => ref.where('uid', '==', user.uid)).valueChanges().subscribe(data => {
+			 console.log(data.length>1);
+			 if(data.length>0){
+				this.removeList(user);
+			 } });
+	 }
+	async removeList(user){
+		await this.delay(300);
+		(this.userList).splice(this.userList.indexOf(user), 1);
+	}
+
+	groupDetailsDisplay(event) {
+		this.selectedGrpName=event.target.value;
+		console.log("selected" + event.target.value)
+		this.getDetails(event.target.value).subscribe(data => {
+			this.grouponSelectedname$ = data;
+		});
+	}
+
+	getAllGroupsCreatedByCurrentUser(uid): Observable<any> {
+		return this.afstore.collection<any>('grouplist', ref => ref.where('createdBy', '==', uid)).valueChanges();
+	}
+	getDetails(grpName): Observable<any> {
+		return this.afstore.collection<any>('adduserstogrp', ref => ref.where('grpname', '==', grpName)).valueChanges();
+	}
 	getUserInfo(firstName): Observable<any> {
 		return this.afstore.collection('users', ref => ref.where('firstName', '==', firstName)).valueChanges();
 	}
-		async cancel() {
-			this.router.navigate(['/home']);
-		}
+	async cancel() {
+		this.router.navigate(['/home']);
+	}
 }
