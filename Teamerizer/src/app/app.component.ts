9fe294e40b36d9e35f2d42081073ef844e7c73de
JohnMads <johnpmadias@gmail.com>
2019-12-15 14:12:44 -0500

    Styling updates

--- Teamerizer/src/app/app.component.ts
+++ Teamerizer/src/app/app.component.ts
@@ -73,7 +73,7 @@ export class AppComponent implements OnInit {
       icon: 'list'
     },
  {
-      title: 'Group Details',
+      title: 'Team Management',
       url: '/groupdetailspage',
       icon: 'list'
     },
