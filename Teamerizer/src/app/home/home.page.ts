5d697dce175e26c7ec6c3d82597f616a6b49cf37
rharkov <rh89333n@pace.edu>
2020-02-04 22:36:58 -0500

    update frommmy group to all group

--- Teamerizer/src/app/home/home.page.ts
+++ Teamerizer/src/app/home/home.page.ts
@@ -4,55 +4,67 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { Router,NavigationExtras } from '@angular/router';
 import {Observable} from 'rxjs';
 import {AngularFireAuth} from '@angular/fire/auth';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  public groupList: any[];
-  uid: string;
-  group$;
-  groupname:string;
-  selectedGrpName:any;
-  constructor(public menu: MenuController, private fireStore: AngularFirestore,public router: Router, public afAuth: AngularFireAuth ) {
 
-  }
+  groupList;
+
+  constructor(public menu: MenuController,
+    public afstore: AngularFirestore,
+        public afAuth: AngularFireAuth,
+        private router: Router,) {}
 
   ngOnInit() {
     this.menu.enable(true);
 
     this.afAuth.authState.subscribe(user => {
       if(user){
-        this.getAllGroupsCreatedByCurrentUser(user.uid).subscribe(data => {
-          console.log("Group List Data:", data);
-          this.group$ = data;
-        });
+        // this.getAllGroupsCreatedByCurrentUser(user.uid).subscribe(data => {
+        //   console.log("Group List Data:", data);
+        //   this.group$ = data;
+        // });
       }
     });
-
-    this.fireStore.collection('grouplist').valueChanges().subscribe(groupList => {
+    
+    this.getAllGroup().subscribe(groupList => {
       this.groupList = groupList;
     })
   }
 
   getAllGroupsCreatedByCurrentUser(uid): Observable<any> {
-    return this.fireStore.collection<any>('grouplist', ref => ref.where('createdBy', '==', uid)).valueChanges()
+    return this.afstore.collection<any>('grouplist', ref => ref.where('createdBy', '==', uid)).valueChanges()
+}
+
+getAllGroup() {
+
+  return this.afstore.collection<any>('grouplist').snapshotChanges().pipe(
+    map(actions => {
+    return actions.map(a => {
+      const data = a.payload.doc.data();
+      const id = a.payload.doc.id;
+      return { id, ...data };
+    });
+    })
+  );
 }
 
   async createGroup(){
     this.router.navigate(['/group-creation'])
   }
 
-  async groupdetail(groupname:string){
+  async groupdetail(group:string){
     
     let navigationExtras: NavigationExtras = {
       state: {
-        groupname:groupname
+        group:group
       }
     };
 
     this.router.navigate(['/groupdetailspage'],navigationExtras)
   }
-
 }
