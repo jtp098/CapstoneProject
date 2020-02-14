c261a5a5538ee69663962be202145462005baa54
JohnMads <johnpmadias@gmail.com>
2020-02-13 20:20:06 -0500

    Updates to login page and profile page

--- Teamerizer/src/app/profile/profile.page.ts
+++ Teamerizer/src/app/profile/profile.page.ts
@@ -4,13 +4,13 @@ import { UserService } from '../user.service';
 import { firestore } from 'firebase/app';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
-import {Observable} from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
+import {Observable} from "rxjs";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.page.html',
-  styleUrls: ['./profile.page.scss'],
+    selector: 'app-profile',
+    templateUrl: './profile.page.html',
+    styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
     mainuser: AngularFirestoreDocument
@@ -21,6 +21,9 @@ export class ProfilePage implements OnInit {
     adminId$;
     adminIds$ = [];
     adminUser$ = [];
+    adminInfo$ = [];
+    adminInfoMul$ ;
+    admincount= -1;
     zip$ = [];
     zip2$ = [];
     username:string
@@ -36,109 +39,126 @@ export class ProfilePage implements OnInit {
 
 
 
-    constructor(private afs: AngularFirestore, private user: UserService, private router: Router, 
-      private afAuth: AngularFireAuth) {
-     
+    constructor(private afs: AngularFirestore, private user: UserService, private router: Router,
+                private afAuth: AngularFireAuth) {
+
     }
 
-  ngOnInit() {
-    let self = this;
-    this.afAuth.auth.onAuthStateChanged(function(user) {
-      console.log("User",user);
-      if (user) {
-        self.setUserProfileData();
-      } else {
-        
-      }
-    });
+    ngOnInit() {
+        let self = this;
+        this.afAuth.auth.onAuthStateChanged(function(user) {
+            console.log("User",user);
+            if (user) {
+                self.setUserProfileData();
+            } else {
 
-    this.afAuth.authState.subscribe(user => {
-      if(user){
-
-        //method uses collection, adduserstogrp
-        this.getAllGroupsUserIsIn(user.uid).subscribe(data => {
-          console.log("Group List Data:", data);
-          this.team$ = data;
-          console.log(data[0].grpname);
-    
-          //method uses collection, groupList to set admin ids
-          this.getAllGroupListData().subscribe(adminData => {
-            this.allGroupListData$ = adminData;
-            for (let i = 0; i < adminData.length; i++) {
-              for(let j = 0; j < this.team$.length; j++) {
-                if(adminData[i].groupname === this.team$[j].grpname) {
-                  this.teamName$ = adminData[i].groupname;
-                  this.adminId$ = adminData[i].createdBy;
-                  console.log("admin id: " + this.adminId$);
-                  this.adminIds$.push(this.adminId$);
-
-                  /*this.zip2$ = this.allGroupListData$.map(o => {
-                    return { team: o.groupname, admin: o.createdBy};
-                  });*/
-        
-                    //console.log("Admin Id Map: " + this.zip2$);
-
-                  //method uses collection, users. Call function to get all admin users
-                  this.getGroupAdminUser(this.adminId$).subscribe(data => {
-                    this.adminUser$.push(data);
-                    console.log("Admin Users " + this.adminUser$);
-
-                    this.zip$ = this.adminUser$.map((x, i) => {
-                      //for(j = 0; j < this.adminIds$.length; j++) {
-                        //if(this.adminId$ === x[0].uid) {
-                          console.log("true!");
-                          console.log(x[0].firstName);
-                          return  [x[0].firstName, this.team$[i].grpname]
-                        //}
-                      //}
-                    });
-          
-                    //console.log("Admin Id Map: " + this.zip$);
-                  });
-                }
-              }
             }
-            console.log(this.adminIds$);
-
-          });
-
         });
-      }
-    });
-  }
 
-  setUserProfileData(){
-    this.mainuser = this.afs.doc(`users/${this.afAuth.auth.currentUser.uid}`)
-    this.sub = this.mainuser.valueChanges().subscribe(event => {
-      this.username = event.username
-      this.firstname = event.firstName
-      this.lastname = event.lastName
-      this.selectedSkill = event.skillType
-      this.selectedLevel = event.skillLevel
-      this.interests =event.interests
-      console.log(this.selectedSkill);
-    })
-  }
+        this.afAuth.authState.subscribe(user => {
+            if(user){
 
-  updateProfile(){
-    this.router.navigate(['/update-profile'])
-  }
+                //method uses collection, adduserstogrp
+                this.getAllGroupsUserIsIn(user.uid).subscribe(data => {
+                    console.log("Group List Data:", data);
+                    this.team$ = data;
+                    console.log(data[0].grpname);
 
-  getAllGroupsUserIsIn(uid): Observable<any> {
-    return this.afs.collection<any>('adduserstogrp', ref => ref.where('uid', '==', uid)).valueChanges()
-  }
+                    //method uses collection, groupList
+                    this.getAllGroupListData().subscribe(adminData => {
+                        this.allGroupListData$ = adminData;
+                        for (let i = 0; i < adminData.length; i++) {
+                            for(let j = 0; j < this.team$.length; j++) {
+                                if(adminData[i].groupname === this.team$[j].grpname) {
+                                    this.teamName$ = adminData[i].groupname;
+                                    this.adminId$ = adminData[i].createdBy;
+                                    console.log("admin id: " + this.adminId$);
+                                 this.getAdminGrpById(this.adminId$);
 
-  getAllGroupListData(): Observable<any> {
-    return this.afs.collection<any>('grouplist').valueChanges()
-  }
+                                    this.adminIds$.push(this.adminId$);
+                                    this.zip2$ = this.allGroupListData$.map(o => {
+                                        return [{ team: o.groupname, admin: o.createdBy}];
+                                    });
 
-  getGroupAdminUser(adminId): Observable<any> {
-    return this.afs.collection<any>('users', ref => ref.where('uid', '==', adminId)).valueChanges()
-  }
+                                    //console.log("Admin Id Map: " + this.zip2$);
+                                    //method uses collection, users
+                                    this.getGroupAdminUser(this.adminId$).subscribe(data => {
+                                      this.adminUser$.push(data);
+                                      console.log("Admin User$" + JSON.stringify(this.adminUser$));
   
+                                      this.adminUser$.map((x, i) => {
+                                          for ( let k = 0; k < this.adminInfo$.length; k++) {
+                                              console.log("Admin Info " + JSON.stringify(this.adminInfo$));
+                                              if(x[0].uid===this.adminIds$[k]){
+                                                  console.log("Last check " + JSON.stringify([{ firstname: x[0].firstName, lastname: x[0].lastName,  grpname: (this.adminInfo$[k])[0].groupname}]) );
+                                                  
+                                                  /*if(this.zip$.indexOf( [{ firstname: x[0].firstName, lastname: x[0].lastName,  grpname: (this.adminInfo$[k])[0].groupname }]) >=-1){*/
+                                                      this.zip$.push( [{ firstname: x[0].firstName, lastname: x[0].lastName, createdby: (this.adminInfo$[k])[0].createdBy, grpname: (this.adminInfo$[k])[0].groupname }]);
 
-  async cancel(){
-    this.router.navigate(['/home'])
-  }
+                                                     // this.zip$.filter((v,i,a) => a.findIndex(t => (JSON.stringify(t) === JSON.stringify(v))) === i);
+                                                //}
+                                              }
+                                              console.log(JSON.stringify(this.zip$));
+                                            
+                                          }
+                                      });
+                                    });
+                                }
+                            }
+                        }
+
+                    });
+
+                });
+
+            }
+        });
+    }
+    //methods
+    getAdminGrpById(uid): any[] {
+        this.getAdminGrpName(uid).subscribe(data  => {
+            this.adminInfoMul$ = data ;
+            this.adminInfo$.push(this.adminInfoMul$);
+            console.log(this.adminInfo$);
+        });
+        return this.adminInfo$;
+    }
+    setUserProfileData(){
+        this.mainuser = this.afs.doc(`users/${this.afAuth.auth.currentUser.uid}`)
+        this.sub = this.mainuser.valueChanges().subscribe(event => {
+            this.username = event.username
+            this.firstname = event.firstName
+            this.lastname = event.lastName
+            this.selectedSkill = event.skillType
+            this.selectedLevel = event.skillLevel
+            this.interests =event.interests
+            console.log(this.selectedSkill);
+        })
+    }
+
+    updateProfile(){
+        this.router.navigate(['/update-profile'])
+    }
+
+    getAllGroupsUserIsIn(uid): Observable<any> {
+        return this.afs.collection<any>('adduserstogrp', ref => ref.where('uid', '==', uid)).valueChanges()
+    }
+
+    getAllGroupListData(): Observable<any> {
+        return this.afs.collection<any>('grouplist').valueChanges()
+    }
+    getAdminGrpName(createdBy): Observable<any> {
+        return this.afs.collection<any>('grouplist', ref => ref.where('createdBy', '==', createdBy)).valueChanges()
+    }
+    getGroupAdminUser(adminId): Observable<any> {
+        return this.afs.collection<any>('users', ref => ref.where('uid', '==', adminId)).valueChanges()
+    }
+
+    getGroupAdminUserDetails(adminId): Observable<any> {
+        return this.afs.collection<any>('adduserstogrp', ref => ref.where('uid', '==', adminId)).valueChanges()
+    }
+    async cancel(){
+        this.router.navigate(['/home'])
+    }
 
 }
