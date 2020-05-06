a4da6f6bc0d532e2bbef50a7d86705ba1596dc23
Vijay <vg54639n@pace.edu>
2020-05-05 22:42:37 -0400

    changed eye icon

--- Teamerizer/src/app/passwordreset/passwordreset.page.ts
+++ Teamerizer/src/app/passwordreset/passwordreset.page.ts
@@ -11,13 +11,16 @@ import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/form
   styleUrls: ['./passwordreset.page.scss'],
 })
 export class PasswordresetPage implements OnInit {
-loginData: { username: '', oldPassword: '', newPassword: '', confirmPassword: '' };
    username: AbstractControl ;
   oldPassword: AbstractControl;
   newPassword: AbstractControl;
   confirmPassword: AbstractControl;
   authReset: FormGroup ;
   passwordtype:string='password';
+  oldpasswordtype:string='password';
+  cnfpasswordtype:string='password';
+  cnfpasseye:string='eye';
+  oldpasseye:string='eye';
   passeye:string ='eye';
 
   // tslint:disable-next-line:max-line-length
@@ -36,16 +39,16 @@ loginData: { username: '', oldPassword: '', newPassword: '', confirmPassword: ''
   async resetPassword() {
     try {
       debugger;
-      console.log(this.username.value,this.oldPassword.value, this.newPassword.value ,this.confirmPassword.value);
+      console.log(this.username.value, this.oldPassword.value, this.newPassword.value , this.confirmPassword.value);
       // tslint:disable-next-line:prefer-const
       let res = await this.afAuth.auth.signInWithEmailAndPassword(this.username.value, this.oldPassword.value);
       if (res.user) {
-        if( this.newPassword.value === this.confirmPassword.value) {
-          await res.user.updatePassword(this.newPassword.value).then(function() {
-            this.router.navigate(['/home']);
-          }).catch(function(error) {
-            // An error happened.
-          });;
+        if( this.newPassword.value === this.confirmPassword.value ) {
+            await res.user.updatePassword(this.newPassword.value).then(function() {
+              this.router.navigate(['/home']);
+            }).catch(function(error) {
+              // An error happened.
+            });
         }
       }
     } catch (error) {
@@ -63,6 +66,23 @@ loginData: { username: '', oldPassword: '', newPassword: '', confirmPassword: ''
       this.passeye = 'eye';
     }
   }
+  showcnfPassword() {
+    if (this.cnfpasswordtype == 'password') {
+      this.cnfpasswordtype = 'text';
+      this.cnfpasseye = 'eye-off';
+    } else {
+      this.cnfpasswordtype = 'password';
+      this.cnfpasseye = 'eye';
+    }
+  }
+  showOldPassword(){
+    if (this.oldpasswordtype == 'password') {
+      this.oldpasswordtype = 'text';
+      this.oldpasseye = 'eye-off';
+    } else {
+      this.oldpasswordtype = 'password';
+      this.oldpasseye = 'eye';
+    }  }
 
   backEvent(){
     this.router.navigate(['/login'])
